0b62814fd6d30b69a9d403b577dbc000cac64b3b
Peter Pod <peterpod@hotmail.com>
2016-01-20 16:12:20 -0500

    Application makes requests successfully to api and updates on new searches

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,30 +4,20 @@
 
 var Controllers = angular.module('Controllers', []);
 
-Controllers.controller('searchCtrl', ['$scope', '$http',
-  function($scope, $http) {
-    $scope.$watch('search', function() {
-      fetch();
-    });
+Controllers.controller('searchCtrl', 
+  function($scope, $http,$timeout) {
 
-    function fetch(){
-      $http.get("http://api.brewerydb.com/v2/search?q=" + $scope.search + "type=beer&key=1c42d0e40968577abeb1cbe8b8ee9d67&format=json")
+    $scope.search = "Yuengling";
+    $scope.beers = " ";
+
+    $scope.fetch = function(){
+      var query = "http://api.brewerydb.com/v2/search?q=" + $scope.search + "&type=beer&key=1c42d0e40968577abeb1cbe8b8ee9d67&format=json";
+      $http.get(query)
       .then(function(response){ 
         $scope.beers = response.data; 
+        console.log(JSON.stringify($scope.beers));
+        $scope.$applyAsync();
+        return $scope.beers;  
       });
     }
-
-    $scope.search = "Sherlock Holmes";
-
-  }]);
-
-// phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
-//   function($scope, $routeParams, Phone) {
-//     $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
-//       $scope.mainImageUrl = phone.images[0];
-//     });
-
-//     $scope.setImage = function(imageUrl) {
-//       $scope.mainImageUrl = imageUrl;
-//     };
-//   }]);
+});
--- app/js/filters.js
+++ app/js/filters.js
@@ -2,7 +2,12 @@
 
 /* Filters */
 
-angular.module('Filters', []);
+angular.module('Filters', [])
+.filter('num', function() {
+    return function(input) {
+      return parseInt(input, 10);
+    };
+});
 // .filter('checkmark', function() {
 //   return function(input) {
 //     return input ? '\u2713' : '\u2718';
