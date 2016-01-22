ffd8de08faa1934fa78302d73edb1643d2514594
Peter Pod <peterpod@hotmail.com>
2016-01-22 14:47:09 -0500

    Added html for login & signup

--- app/js/app.js
+++ app/js/app.js
@@ -10,6 +10,11 @@ var beerify = angular.module('beerify', [
   'Services'
 ]);
 
+beerify.run(function($rootScope) {
+    Parse.initialize("IsRz8bEqA5zitaXPrvi8Pd2UZMxKOKiSfCizEosG", "hckGgFl5lDoKEtktpPdlX4h5Q1abI6xD9RjnpeVL");
+    $rootScope.sessionUser = Parse.User.current();
+ });
+
 beerify.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
